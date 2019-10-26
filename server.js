1854506fff9c936a0f08d9e4185590b3b722c919
quotto <rhyme.and.rhyme@gmail.com>
2019-10-27 00:01:40 +0900

    Heroku動作のため環境変数からポートを取得するように修正

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 
 const app = express();
-const server = http.createServer(app).listen(2000,()=>{
+const server = http.createServer(app).listen(proecss.env.PORT || 2000,()=>{
     console.log('server start on port:' + server.address().port);
 });
 app.use('/', express.static('./public'));
