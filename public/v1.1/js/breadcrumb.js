f32827f7ac46c5123c29fac2eb2be8cc1ef5c184
quotto <rhyme.and.rhyme@gmail.com>
2020-01-23 23:51:38 +0900

    はてなブログ公式でjson+ldが挿入されているので一旦実装から削除します。

--- public/v1.1/js/breadcrumb.js
+++ public/v1.1/js/breadcrumb.js
@@ -1,14 +1,4 @@
 var host = $(location).attr('host');
-var jsonld = {
-  '@context': 'https://schema.org',
-  '@type': 'BreadcrumbList',
-  'itemListElement':[{
-    '@type': 'ListItem',
-    'position': 1,
-    'name': 'トップ',
-    'item': 'https://'+host
-  }]
-}
 function remapBreadcrumb(breadcrumb){
   new_breadcrumb_html='';
   for(var i=0;i<breadcrumb.length;i++) {
@@ -21,20 +11,10 @@ function remapBreadcrumb(breadcrumb){
     if(i+1<breadcrumb.length) {
       new_breadcrumb_html += '<span class="breadcrumb-gt"> &gt;</span>';
     }
-    jsonld.itemListElement.push({
-      '@type': 'ListItem',
-      'position': i+2,
-      'name': url_category[i],
-      'item': category_url
-    });
   }
   if(new_breadcrumb_html != '') {
     $('#top-box > div.breadcrumb > div.breadcrumb-inner > span.breadcrumb-child:first').prop('outerHTML',new_breadcrumb_html);
   }
-
-  var jsonldTag = $('<script type="application/ld+json"></script>');
-  jsonldTag.append(JSON.stringify(jsonld));
-  $('title').after(jsonldTag);
 }
 
 function remapArticleCategory(categories) {
@@ -65,28 +45,13 @@ function remapCategoryBreadcrumb(breadcrumb) {
       new_breadcrumb_html += '<span class="breadcrumb-child"><a class="breadcrumb-child-link" href="'+category_url+'">'+breadcrumb[i]+'</span></a></span>';
       new_breadcrumb_html += '<span class="breadcrumb-gt"> &gt;</span>';
       
-      jsonld.itemListElement.push({
-        '@type': 'ListItem',
-        'position': i+2,
-        'name': url_category[i],
-        'item': category_url
-      });
     } else {
       new_breadcrumb_html += '<span class="breadcrumb-child">'+breadcrumb[i]+'</span>';
-      jsonld.itemListElement.push({
-        '@type': 'ListItem',
-        'position': i+2,
-        'name': breadcrumb[i],
-        'item': location.href
-      });
     }
   }
   if(new_breadcrumb_html != '') {
     $('#top-box > div.breadcrumb > div.breadcrumb-inner > span.breadcrumb-child:first').prop('outerHTML',new_breadcrumb_html);
   }
-  var jsonldTag = $('<script type="application/ld+json"></script>');
-  jsonldTag.append(JSON.stringify(jsonld));
-  $('title').after(jsonldTag);
 }
 
 function remapArchiveCategory(categories) {
